0df4caf87268b1fea4bd2880fffc7c903fbb794d
yahi <youcefyahi1994@gmail.com>
2022-07-07 17:06:02 +0200

    seond  deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
+
 const App = () =>{
 return(
 <BrowserRouter>
