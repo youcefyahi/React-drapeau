9ff9e6d5ceaa6b6933934c5fd7f5361e519c8301
yahi <youcefyahi1994@gmail.com>
2022-07-07 15:58:44 +0200

    un  commit

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Countries from "../components/Countries";
+import Navigation from "../components/Navigation";
+
+const Home = () => {
+    return (
+        <div>
+            <Navigation />
+            <Countries/>
+        </div>
+
+    )
+}
+
+export default Home;
+
